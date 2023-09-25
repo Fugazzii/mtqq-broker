12e4684432497ad1344b769dfa6fa3e4ebc89bdc
Fugazzii <sichinavailia@gmail.com>
2023-09-25 12:47:26 +0400

    Implemented Consumer on high-level

---
+++ src/bin/consumer.rs
@@ -0,0 +1,20 @@
+use tokio::net::TcpStream;
+
+struct Consumer {
+    broker: TcpStream
+}
+
+impl Consumer {
+
+    pub fn new() {}
+
+    pub fn subscribe() {}
+
+    pub fn unsubscribe() {}
+    
+}
+
+#[tokio::main]
+pub async fn main() {
+
+}
