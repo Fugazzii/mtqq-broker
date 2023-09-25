446ed3bc845ec30caae4c53f0e9f57572e4fac3d
Fugazzii <sichinavailia@gmail.com>
2023-09-25 14:11:59 +0400

    Separated Topic implementations

--- src/topic.rs
+++ src/topic.rs
@@ -3,22 +3,29 @@ use crate::traits::topic::BrokerTopic;
 
 #[derive(Debug)]
 pub struct Topic {
+    name: String,
     queue: VecDeque<String>
 }
 
 impl Topic {
-    pub fn new() -> Self {
-        Topic { queue: VecDeque::new() }
+    pub fn new(name: &str) -> Self {
+        Topic { name: name.into(), queue: VecDeque::new() }
     }
 }
 
 impl BrokerTopic for Topic {
 
-    fn publish(&mut self, message: &str) {
+    fn publish(&mut self, name: &str, message: &str) {
+
+        if name.to_string() != self.name { panic!("Invalid name") }
+
         self.queue.push_back(message.into());
     }
 
-    fn consume(&mut self) -> Option<String> {
+    fn consume(&mut self, name: &str) -> Option<String> {
+
+        if name.to_string() != self.name { panic!("Invalid name") }
+
         if let Some(message) = self.queue.pop_back() {
             Some(message)
         } else {
